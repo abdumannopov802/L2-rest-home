31bde36caa9305886142562b03b58eaf831ff14a
abdumannopov802 <154593414+abdumannopov802@users.noreply.github.com>
2024-03-26 17:20:31 +0500

    authenticate user

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import EmployeeList, EmployeeDetail, DepartmentList, DepartmentDetail
+from .views import *
 
 urlpatterns = [
-    path('employee/', EmployeeList.as_view()),
+    path('employee/', EmployeeList.as_view(), name='employee_list'),
     path('employee/<int:pk>/', EmployeeDetail.as_view()),
-    path('department/', DepartmentList.as_view()),
+    path('department/', DepartmentList.as_view(), name='department_list'),
     path('department/<int:pk>/', DepartmentDetail.as_view()),
-
+    # user actions
+    path('', home, name='home'),
+    path('login/', user_login, name='login'),
+    path('signup/', user_signup, name='signup'),
+    path('logout/', user_logout, name='logout'),
 ]
